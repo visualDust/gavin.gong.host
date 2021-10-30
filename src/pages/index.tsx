7af2946c69c55a60fb412a259e039db8fa443599
VisualDust <miya@akasaki.space>
2021-10-30 18:00:32 +0800

    whatever

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
 import HomepageFeatures from "../components/HomepageFeatures";
+import Typical from "react-typical";
 
 function HomepageBackground() {
   const { siteConfig } = useDocusaurusContext();
@@ -12,10 +13,19 @@ function HomepageBackground() {
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <div className="row">
-          <div className="col col--8" style={{alignSelf:"center", marginBottom:"50px"}}>
+          <div className="col col--8" style={{ alignSelf: "center", marginBottom: "50px" }}>
             <h1 className="hero__title">{siteConfig.title}</h1>
             <p className="hero__subtitle">
-             Rubbish CVer、Poor LaTex speaker、Half stack developer
+              <Typical
+                steps={[
+                  'Rubbish CVer、Poor LaTex speaker、Half stack developer',
+                  2000,
+                  'VusualDust',
+                  1000,
+                ]}
+                loop={Infinity}
+                wrapper="span"
+              />
             </p>
             <div>
               <a className="button button--outline" href="/docs">Blogs</a>
