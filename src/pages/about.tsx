d5f9f6fa0b604ef8d4ba88627ac1be74c545f6ab
Gavin Gong <Miya@Akasaki.space>
2022-01-15 09:34:05 +0800

    whatever

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
+import arrayShuffle from "array-shuffle";
 
 function About() {
   return (
     <Layout>
       <Friends />
+      <p style={{paddingLeft:'20px'}}>The list is random. try to refresh the page.</p>
     </Layout>
   );
 }
@@ -18,120 +20,69 @@ interface FriendData {
   note: string;
 }
 
-const friendsData: FriendData[] = [
-  {
-    pic: "https://avatars.githubusercontent.com/u/33346934?v=4",
-    name: "ViusalDust",
-    intro: "Rubbish CVer | Poor LaTex speaker | Half stack developer | 键圈躺尸砖家",
-    url: "https://github.com/visualDust",
-    note: "Building random things with Deno, Node and .NET Core.",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-  {
-    pic: "https://avatars.githubusercontent.com/u/14901890?v=4",
-    name: "lideming",
-    intro: "Building random things with Deno, Node and .NET Core.",
-    url: "test2",
-    note: "test2",
-  },
-];
+function getFriendsData(): FriendData[] {
+  var data: FriendData[] = [
+    {
+      pic: "https://github.yuuza.net/lideming.png",
+      name: "lideming",
+      intro: "Building random things with Deno, Node and .NET Core.",
+      url: "https://yuuza.net/",
+      note: "我的大学同学，某些时候是同事，也有时是队友。全栈/CSGO/EPICS/BFV，有些懒。",
+    },
+    {
+      pic: "https://github.yuuza.net/papercube.png",
+      name: "PaperCube",
+      intro: "我是废物",
+      url: "https://github.com/papercube",
+      note: "我的初中高中同学，我在玩泥巴的时候他就在写java了；语言学砖家（各种意义上的）。我们常打MC。",
+    },
+    {
+      pic: "https://github.yuuza.net/VoidmatrixHeathcliff.png",
+      name: "Voidmatrix",
+      intro: "积淀，分享，传承",
+      url: "http://voidmatrix.xyz/",
+      note: "素未谋面的大佬，轮子从galgame引擎造到前端，雨露均沾。我们假期一起打MC。",
+    },
+    {
+      pic: "https://github.yuuza.net/redhat123456.png",
+      name: "Tanger",
+      intro: "赶紧学习",
+      url: "https://tanger.ltd/",
+      note: "隔壁学院（数学与计算科学）基地的负责人。常见面交流。",
+    },
+    {
+      pic: "https://github.yuuza.net/Therainisme.png",
+      name: "Therainisme",
+      intro: "赶紧学习",
+      url: "https://notebook.therainisme.com/",
+      note: "雨神，一出门就下雨。我大学期间的同事，任我社2019级多媒体部部长。网站开发运维专家。",
+    },
+    {
+      pic: "https://github.yuuza.net/coronaPolvo.png",
+      name: "coronaPolvo",
+      intro: "life is but a span, I use python",
+      url: "https://blog.keter.top/",
+      note: "科协F4的成员。我大学期间的同事，任我社2019级软件部部长。主攻方向是神经网络攻击。他找女朋友之前我们常一起吃饭。",
+    },
+    {
+      pic: "https://github.yuuza.net/Zerorains.png",
+      name: "Zerorains",
+      intro: "life is but a span, I use python",
+      url: "blog.zerorains.top",
+      note: "科协F4的成员。科协恶霸(x)。主攻方向是基于深度学习技术图像分割。他不太喜欢看我抽卡。",
+    },
+    {
+      pic: "https://github.yuuza.net/PommesPeter.png",
+      name: "PommesPeter",
+      intro: "I want to be strong. But it seems so hard.",
+      url: "https://blog.pommespeter.com/",
+      note: "科协F4的成员。我大学期间的同事，任我社2019级软件部部长。主攻方向是神经网络攻击。他找女朋友之前我们常一起吃饭。",
+    },
+  ];
+  return arrayShuffle(data);
+}
+
+const friendsData = getFriendsData();
 
 function Friends() {
   const [current, setCurrent] = useState(0);
@@ -139,23 +90,29 @@ function Friends() {
   useEffect(() => {
     return () => {
       setPrevious(current);
-    }
-  }, [current])
+    };
+  }, [current]);
   return (
     <div className="friends">
-      <div style={{ position: 'relative' }}>
+      <div style={{ position: "relative" }}>
         <div className="friend-columns">
           {/* Big card showing current selected */}
           <div className="friend-card-outer">
-            {previous != current && <FriendCard key={previous} data={friendsData[previous]} fadeout />}
+            {previous != current && (
+              <FriendCard key={previous} data={friendsData[previous]} fadeout />
+            )}
             <FriendCard key={current} data={friendsData[current]} />
           </div>
 
           <div className="friend-list">
             {friendsData.map((x, i) => (
-              <div key={x.name} className={clsx('friend-item', {
-                current: i == current
-              })} onClick={() => setCurrent(i)}>
+              <div
+                key={x.name}
+                className={clsx("friend-item", {
+                  current: i == current,
+                })}
+                onClick={() => setCurrent(i)}
+              >
                 <img src={x.pic} alt="user profile photo" />
               </div>
             ))}
@@ -166,7 +123,7 @@ function Friends() {
   );
 }
 
-function FriendCard(props: { data: FriendData, fadeout?: boolean }) {
+function FriendCard(props: { data: FriendData; fadeout?: boolean }) {
   const { data, fadeout = false } = props;
   return (
     <div className={clsx("friend-card", { fadeout })}>
@@ -181,14 +138,10 @@ function FriendCard(props: { data: FriendData, fadeout?: boolean }) {
         <div className="card__body">
           <h2>{data.name}</h2>
           <p>
-            <big>
-              {data.intro}
-            </big>
+            <big>{data.intro}</big>
           </p>
           <p>
-            <small>
-              {data.note}
-            </small>
+            <small>Comment : {data.note}</small>
           </p>
         </div>
         <div className="card__footer">
@@ -198,7 +151,7 @@ function FriendCard(props: { data: FriendData, fadeout?: boolean }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default About;
