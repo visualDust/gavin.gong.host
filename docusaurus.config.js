bee2b63ce7b7bedac83f272c6c9e089ac07babad
Gavin Gong <Miya@Akasaki.space>
2022-01-20 22:37:43 +0800

    whatever

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,40 +1,40 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Akasaki Focusing',
-  tagline: 'Empoered learning life',
-  url: 'https://focus.akasaki.space',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/logo.svg',
-  organizationName: 'visualDust', // Usually your GitHub org/user name.
-  projectName: 'focus.akasaki.space', // Usually your repo name.
+  title: "Akasaki Focusing",
+  tagline: "Empoered learning life",
+  url: "https://focus.akasaki.space",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/logo.svg",
+  organizationName: "visualDust", // Usually your GitHub org/user name.
+  projectName: "focus.akasaki.space", // Usually your repo name.
 
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          //todo
-          editUrl: 'https://github.com/visualDust/focus.akasaki.space/edit/main/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl:
+            "https://github.com/visualDust/focus.akasaki.space/edit/master/",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           editUrl:
-            'https://github.com/visualDust/focus.akasaki.space/edit/main/website/blog/',
+            "https://github.com/visualDust/focus.akasaki.space/edit/master/",
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -52,16 +52,16 @@ const config = {
         language: ["en", "zh"],
         // ```
         // When applying `zh` in language, please install `nodejieba` in your project.
-        translations:{
-          "search_placeholder": "Search",
-          "see_all_results": "See all results",
-          "no_results": "No results.",
-          "search_results_for": "Search results for \"{{ keyword }}\"",
-          "search_the_documentation": "Search the documentation",
-          "count_documents_found": "{{ count }} document found",
-          "count_documents_found_plural": "{{ count }} documents found",
-          "no_documents_were_found": "No documents were found"
-        }
+        translations: {
+          search_placeholder: "Search",
+          see_all_results: "See all results",
+          no_results: "No results.",
+          search_results_for: 'Search results for "{{ keyword }}"',
+          search_the_documentation: "Search the documentation",
+          count_documents_found: "{{ count }} document found",
+          count_documents_found_plural: "{{ count }} documents found",
+          no_documents_were_found: "No documents were found",
+        },
       },
     ],
   ],
@@ -70,80 +70,84 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Akasaki Focusing',
+        title: "Akasaki Focusing",
         logo: {
-          alt: 'Logo',
-          src: 'img/logo.svg',
+          alt: "Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
           },
-          {to: '/blog/tags', label: 'Blogs', position: 'left'},
+          { to: "/blog/tags", label: "Blogs", position: "left" },
           {
-            href: 'https://ml.akasaki.space',
-            label: 'MechineLearning Part',
-            position: 'left',
-          },
-          {
-            href: '/about',
-            label: 'About & Links',
-            position: 'right',
+            href: "https://ml.akasaki.space",
+            label: "MechineLearning Part",
+            position: "left",
           },
           {
-            href: 'https://github.com/VisualDust',
-            label: 'Me on GitHub',
-            position: 'right',
+            href: "/about",
+            label: "About & Links",
+            position: "right",
+          },
+          {
+            href: "https://github.com/VisualDust",
+            label: "Me on GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'light',
+        style: "light",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Documents',
-                to: '/docs/intro',
+                label: "Documents",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'NEET CV',
-                href: 'https://github.com/neet-cv',
+                label: "NEET CV",
+                href: "https://github.com/neet-cv",
               },
               {
-                label: 'sanyuankexie',
-                href: 'https://github.com/sanyuankexie',
+                label: "sanyuankexie",
+                href: "https://github.com/sanyuankexie",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'ml.akasaki.space',
-                to: 'https://ml.akasaki.space',
+                label: "ml.akasaki.space",
+                to: "https://ml.akasaki.space",
               },
               {
-                label: 'See me on Github',
-                href: 'https://github.com/VisualDust',
+                label: "See me on Github",
+                href: "https://github.com/VisualDust",
               },
               {
-                label: 'Powered by docusaurus',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "Powered by docusaurus",
+                href: "https://github.com/facebook/docusaurus",
               },
             ],
           },
         ],
-        copyright: `${new Date().getFullYear()}  @VisualDust (aka ${()=>{return "Miya Akasaki"}} aka ${()=>{return "Gavin Gong"}}).`,
+        copyright: `${new Date().getFullYear()}  @VisualDust (aka ${() => {
+          return "Miya Akasaki";
+        }} aka ${() => {
+          return "Gavin Gong";
+        }}).`,
       },
       prism: {
         theme: lightCodeTheme,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
 import BrowserOnly from "@docusaurus/BrowserOnly";
@@ -107,18 +107,18 @@ function HomepageBackground() {
                 className="button button--outline"
                 style={{ border: "solid 1px" }}
                 href="#functionals"
-                onClick={
-                  (e) => {
-                    e.preventDefault();
-                    const target = (e.target as HTMLElement).getAttribute('href');
-                    const yLocation = (document.querySelector(target) as HTMLElement).offsetTop;
-                    // console.log(element)
-                    window.scrollTo({
-                      left: 0,
-                      top: yLocation - 60
-                    })
-                  }
-                }
+                onClick={(e) => {
+                  e.preventDefault();
+                  const target = (e.target as HTMLElement).getAttribute("href");
+                  const yLocation = (
+                    document.querySelector(target) as HTMLElement
+                  ).offsetTop;
+                  // console.log(element)
+                  window.scrollTo({
+                    left: 0,
+                    top: yLocation - 60,
+                  });
+                }}
               >
                 Things Fun
               </a>
@@ -160,7 +160,7 @@ function carousel() {
       swipeable={false}
     >
       {config.illustrations.map((item) => (
-        <img src={item} />
+        <img key={item} src={item} />
       ))}
     </Carousel>
   );
@@ -190,19 +190,114 @@ function WhereAndWhat() {
   );
 }
 
+function useHidingFooter() {
+  useEffect(() => {
+    const footer = document.querySelector(".footer") as HTMLElement;
+    const docEle = document.documentElement;
+    footer.style.transition = "opacity .3s";
+    footer.style.opacity = "0";
+
+    footer.style.display = "none";
+
+    let shown = false;
+    let wheelPaused = false;
+
+    function pauseWheel() {
+      wheelPaused = true;
+      setTimeout(() => {
+        wheelPaused = false;
+      }, 500);
+    }
+
+    const onWheel = (e: WheelEvent) => {
+      if (wheelPaused) {
+        e.preventDefault();
+        return;
+      }
+      if (
+        !shown &&
+        docEle.scrollTop > docEle.scrollHeight - window.innerHeight - 2 &&
+        e.deltaY > 0
+      ) {
+        e.preventDefault();
+        shown = true;
+        footer.style.display = "";
+        footer.style.opacity = "1";
+        docEle.scrollTo({
+          top: docEle.scrollHeight,
+          behavior: "smooth",
+        });
+        pauseWheel();
+      }
+      if (shown && e.deltaY < 0) {
+        e.preventDefault();
+        shown = false;
+        footer.style.opacity = "0";
+        docEle.scrollTo({
+          top: footer.offsetTop - window.innerHeight + 1,
+          behavior: "smooth",
+        });
+        setTimeout(() => {
+          footer.style.display = "none";
+        }, 500);
+        pauseWheel();
+      }
+    };
+
+    window.addEventListener("mousewheel", onWheel, {
+      passive: false,
+      capture: true,
+    });
+    return () => {
+      window.removeEventListener("mousewheel", onWheel, false);
+    };
+
+    // let hasScrolledToMostBottom = false;
+    // let hasReachedFooter = false;
+    // const onScroll = function (e: Event) {
+    //   if (!hasReachedFooter)
+    //     setTimeout(() => {
+    //       hasReachedFooter = true;
+    //     }, 500);
+    //   else if (
+    //     docEle.scrollTop == footer.offsetTop - window.innerHeight ||
+    //     docEle.scrollTop > footer.offsetTop - window.innerHeight + 100
+    //   ) {
+    //     if (!hasScrolledToMostBottom) {
+    //       e.preventDefault();
+    //       hasScrolledToMostBottom = true;
+    //       footer.style.opacity = "1";
+    //       docEle.scrollTo({
+    //         top: footer.offsetTop,
+    //         behavior: "smooth",
+    //       });
+    //     }
+    //   }
+    // };
+
+    // window.addEventListener("scroll", onScroll);
+    // return () => {
+    //   window.removeEventListener("scroll", onScroll);
+    // };
+  }, []);
+}
+
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
+  useHidingFooter();
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="Akasaki Focusing is where Gavin Gong (aka VisualDust) enjoys coding life"
     >
       <HomepageBackground />
-      <main id="functionals"
+      <main
+        id="functionals"
         style={{
           display: "flex",
           minHeight: "calc(100vh - var(--ifm-navbar-height))",
-        }}>
+        }}
+      >
         <BrowserOnly>{FeatureSwiper}</BrowserOnly>
         {/* <Parallax bgImage={bgimg} strength={500}>
           <div style={{ height: 500 }}>
@@ -213,6 +308,16 @@ export default function Home(): JSX.Element {
       </main>
     </Layout>
   );
+}
+
+function AsyncComponent(props: { func: () => Promise<JSX.Element> }) {
+  const [loaded, setLoaded] = useState<JSX.Element>(null);
+  useEffect(() => {
+    props.func().then((result) => {
+      setLoaded(result);
+    });
+  }, []);
+  return loaded ? loaded : <div>Loading...</div>;
 }
 
 function FeatureSwiper(): JSX.Element {
