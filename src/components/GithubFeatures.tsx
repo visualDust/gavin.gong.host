65d0881719f496cd0ee860c74e4492b6c43e8fd2
VisualDust <gavin@gong.host>
2023-03-16 17:40:54 +0800

    fixed center alignment for github recent activities

--- src/components/GithubFeatures.tsx
+++ src/components/GithubFeatures.tsx
@@ -6,11 +6,11 @@ export function GithubFeatures(): JSX.Element {
     <div style={{ height: '100%'}}>
       <div className="container">
         <div className="column" style={{ height: "100%", alignItems: "end" }}>
-          <div className="text--center col col--6">
+          <div className="text--center">
             <h1 className="h2">Recent activities</h1>
           </div>
-          <div className="text--center col col--6">
-            <img src="https://ghchart.rshah.org/409ba5/visualdust" alt="" />
+          <div className="text--center">
+            <img src="https://ghchart.rshah.org/visualdust" alt="" />
           </div>
         </div>
       </div>
