1cacd793d045208e83ef7d59b7d59b5ee06a76e8
therainisme <therainisme@qq.com>
2023-08-09 18:29:32 +0800

    KD 1.16

--- src/pages/_index.config.ts
+++ src/pages/_index.config.ts
@@ -8,6 +8,7 @@ export const subtitles_and_delays: TypicalText[] = [
   { text: "Poor LaTex speaker", delay: 1000 },
   { text: "Half stack developer", delay: 1000 },
   { text: "VisualDust", delay: 1000 },
+  { text: "KD 1.16", delay: 1000 },
 ];
 
 //should be in [0,12]
